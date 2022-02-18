c40f4e32d2ae459211be4f9a740fc4e73bd5ee15
Nataliaum11 <99738055+Nataliaum11@users.noreply.github.com>
2022-02-17 22:07:07 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Tarea_LPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int respuesta;
+            double valorQuincenal, bonoProfesionalizacion, bonoTransporte, bonoAlimentación, totalSueldo, valorquincena;
+            valorQuincenal = 1050000;
+            bonoProfesionalizacion = (valorQuincenal / 100) * 10;
+            bonoTransporte = 120000;
+            bonoAlimentación = (25000 * 15);
+
+
+            Console.WriteLine("Por favor ingrese 1 si es profesional, de lo contrario, por favor ingrese 2");
+            respuesta = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (respuesta == 1)
+            {
+                valorquincena = valorQuincenal + bonoProfesionalizacion + bonoTransporte + bonoAlimentación;
+
+                totalSueldo = valorquincena - ((valorquincena / 100) * 7);
+            }
+            else
+            {
+                valorquincena = valorQuincenal + bonoTransporte + bonoAlimentación;
+
+                totalSueldo = valorquincena - ((valorquincena / 100) * 7);
+
+            }
+
+            Console.WriteLine("El total que recibirá al final de la quincena será " + totalSueldo);
+
+
+        }
+    }
+    
+}
